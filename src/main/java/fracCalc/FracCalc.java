45378f23e89b8b7e28f6dd7689b6db4690549527
s-muralidharank <s-muralidharank@53S500042105.it.bsd405.org>
2019-11-15 15:28:36 -0800

    Finished Checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -3,13 +3,26 @@
  */
 
 package fracCalc;
+import java.util.Scanner;
 
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	
+    	//System.out.println("Enter a fraction experession");
+    	//String fraction = userInput.nextLine();
+    	
+    	//System.out.println(produceAnswer(fraction));
+    	String fraction = "";
+    	while (fraction != "quit") {
+    		Scanner userInput = new Scanner(System.in);
+    		System.out.println("Enter a fraction experession");
+    		fraction = userInput.nextLine();
+    		if (fraction != "quit") {
+    			System.out.println(produceAnswer(fraction));
+    		}
+    	}
     }
 
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -23,10 +36,37 @@ public class FracCalc {
     public static String produceAnswer(String input)
     {
         // TODO: Implement this function to produce the solution to the input
-
-        return "";
+    	int end;
+    	int lastNumStart;
+    	int space;
+    	int firstNumEnd;
+    	int operatorIndex;
+    	String lastNumber;
+    	String firstNumber;
+    	String operator;
+    	
+    	end = input.length();
+    	space = input.indexOf(" ");
+    	lastNumStart = space + 3;   	
+    	firstNumEnd = space;   	
+    	operatorIndex = input.indexOf(" ") + 1;
+    	
+    	operator = input.substring(operatorIndex, (operatorIndex + 1));
+    	firstNumber = input.substring(0, firstNumEnd);
+    	lastNumber = input.substring(lastNumStart, end);
+    	
+        return lastNumber;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
 
 }
+
+
+
+
+
+
+
+
+;
